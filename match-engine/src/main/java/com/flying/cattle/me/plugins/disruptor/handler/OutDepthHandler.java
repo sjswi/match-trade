4e566282d673bc8c50dd6337d5b31d1aff7c0621
bianpeng <695429818@qq.com>
2020-02-28 11:32:31 +0800

    解决市价吃单bug

--- match-engine/src/main/java/com/flying/cattle/me/plugins/disruptor/handler/OutDepthHandler.java
+++ match-engine/src/main/java/com/flying/cattle/me/plugins/disruptor/handler/OutDepthHandler.java
@@ -11,6 +11,8 @@ import com.flying.cattle.me.util.SpringContextUtils;
 import com.flying.cattle.mt.entity.MatchOrder;
 import com.lmax.disruptor.EventHandler;
 
+import ch.qos.logback.core.joran.conditional.IfAction;
+
 /**
  * @ClassName: OutDepthHandler
  * @Description: TODO(已挂单深度处理消费者)
@@ -22,9 +24,22 @@ public class OutDepthHandler implements EventHandler<MatchOrder> {
 	// EventHandler的方法 - 都消费
 	public void onEvent(MatchOrder order, long sequence, boolean endOfBatch) throws Exception {
 		//当订单是1或2时，说明吃过单，造成了出单深度变化
-		if (order.getState().intValue()==1||order.getState().intValue()==2) {
+		if (this.isEatOrder(order)) {
 			MatchDetailHandler matchDetailHandler = (MatchDetailHandler) SpringContextUtils.getBean("matchDetailHandler");
 			matchDetailHandler.outMatchDepth(order);
+		}
+	}
+	
+	//判断是否吃过单
+	private Boolean isEatOrder(MatchOrder order) {
+		if (order.getIsMarket()) {
+			return order.getList().size()>0?true:false;
+		}else {
+			if (order.getState().intValue()==1||order.getState().intValue()==2) {
+				return true;
+			}else {
+				return false;
+			}
 		}
 	}
 }
